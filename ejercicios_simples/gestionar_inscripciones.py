b16d942890b63af6c9a436ee020efc2f5336a8b2
jeri1991 <joesruis6@hotmail.com>
2025-05-15 20:42:10 -0600

    adding new exercise file (still incomplete)

---
+++ ejercicios_simples/gestionar_inscripciones.py
@@ -0,0 +1,9 @@
+# Función que registra estudiantes que se inscriben en un taller 
+# Toma en cuenta cuántos estudiantes se han inscrito y verifica que no haya duplicados
+# Devuelve los resultados ordenados alfabéticamente
+
+def gestionar_inscripciones():
+    # lista que recibe los nombres a inscribir
+    estudiantes_inscritos = []
+
+# este ejercicio aún está incompleto
